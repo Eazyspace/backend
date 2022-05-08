7740ed087141a6115fefaacd97112a87c7419cc4
huyvu2001 <41413347+huyvu2001@users.noreply.github.com>
2022-05-08 21:10:02 +0700

    Revert "Revert "create account & sign up for students added""
    This reverts commit 54ce7a13ff4fd9b16d50a76fe410908abe9b3fea.

--- model/token.go
+++ model/token.go
@@ -10,6 +10,7 @@ type Token struct {
 	UserID      int64     `json:"userId" gorm:"primaryKey"`
 	User        User      `gorm:"foreignKey:UserID"`
 	ExpireTime  time.Time `json:"expireTime,omitempty"`
+	Role        int64     `josn:"role,omitempty"`
 	TokenString string    `json:"tokenString,omitempty"`
 	jwt.StandardClaims
 }
--- service/admin-service.go
+++ service/admin-service.go
@@ -1,18 +1,40 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/Eazyspace/model"
 	"github.com/Eazyspace/repo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AdminService struct {
 	requestRepository *repo.RequestRepository
+	userRepository    *repo.UserRepository
 }
 
-func NewAdminService(requestRepository *repo.RequestRepository) *AdminService {
-	return &AdminService{requestRepository: requestRepository}
+func NewAdminService(requestRepository *repo.RequestRepository, userRepository *repo.UserRepository) *AdminService {
+	return &AdminService{requestRepository: requestRepository, userRepository: userRepository}
 }
 
 func (s *AdminService) UpdateStatus(request *model.Request) (*model.Request, error) {
 	return s.requestRepository.UpdateStatus(request)
+}
+
+func (s *AdminService) Create(user *model.User) (*model.User, error) {
+	if len(user.AcademicID) == 0 || len(user.Password) == 0 || user.Role < 1 || user.Role > 3 {
+		return nil, errors.New("Missing academicId or password or invalid role")
+	}
+
+	if user.OrganizationID == 0 {
+		user.OrganizationID = 1
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	user.Password = string(hashedPassword)
+
+	return s.userRepository.Create(user)
 }
